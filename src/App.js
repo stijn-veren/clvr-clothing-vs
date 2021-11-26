ecd984f6ff580da302f3a30179ba2dda859aa516
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-26 18:50:37 +0200

    Add heading using react-icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { GiClover } from "react-icons/gi";
 import './App.css'
 
 function App() {
-  return <div>App</div>
+  return (<div>
+    <h1><GiClover /> CLVR Clothing VS</h1>
+  </div>)
 }
 
 export default App
