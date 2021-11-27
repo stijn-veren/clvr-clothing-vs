4f2053e2637aa6720cd31c9f3737395430a5e7a5
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-27 02:55:58 +0200

    Add svg logo and import it as a component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+// components
+import { ReactComponent as Logo } from './assets/clvr-clothing-logo.svg'
+
 // styles
 import './App.sass'
 
 function App() {
-  return <div>Hello, World!</div>
+  return (
+    <div>
+      <Logo />
+      Hello, World!
+    </div>
+  )
 }
 
 export default App
