8278cd11025e65a611b0648ec902202f5932a989
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-27 03:52:03 +0200

    Add catalogue data

---
+++ src/assets/catalogue.data.js
@@ -0,0 +1,9 @@
+const CATALOGUE_DATA = [
+  { id: 0, name: 'Hats' },
+  { id: 1, name: 'Jackets' },
+  { id: 2, name: 'Sneakers' },
+  { id: 3, name: 'Womens' },
+  { id: 4, name: 'Mens' },
+]
+
+export default CATALOGUE_DATA
