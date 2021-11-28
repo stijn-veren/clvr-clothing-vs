4e1bdb91ac3a2751e19bc82411aee71a1f84ce28
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-29 01:09:37 +0200

    Add app data

---
+++ src/assets/app.data.js
@@ -0,0 +1,14 @@
+const APP_DATA = {
+  title: 'CLVR Clothing',
+  fullName: 'CLVR Clothing by Veren Studio',
+  copyright: 'Copyright 2021',
+  phone: '+380 (123) 456-78-90',
+  email: 'clvr.clothing@gmail.com',
+  menu: [
+    { title: 'Shop', link: 'shop' },
+    { title: 'About', link: 'about' },
+    { title: 'Contacts', link: 'contacts' },
+  ],
+}
+
+export default APP_DATA
