7ed1f992b7deb1d0c12899e20173774c1b9d1ba3
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-29 03:59:53 +0200

    Fix some styles

--- src/components/nav-link/nav-link.styles.js
+++ src/components/nav-link/nav-link.styles.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-export const Link = styled.a`
+export const LinkStyled = styled(Link)`
   color: ${({ active }) => (active ? 'tomato' : '#333')};
   text-decoration: none;
   padding: 5px 15px;
   text-transform: uppercase;
   transition: color 0.5s;
 
-  &:hover, &:active {
+  &:hover,
+  &:active {
     color: tomato;
   }
 `
--- src/components/navigation/navigation.styles.js
+++ src/components/navigation/navigation.styles.js
@@ -1,5 +1,3 @@
 import styled from 'styled-components'
 
-export const Nav = styled.nav`
-
-`
+export const Nav = styled.nav``
