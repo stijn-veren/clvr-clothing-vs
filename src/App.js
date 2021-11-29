61e2c7e567dbbe43ed86cd1f36e117892900fa72
Kostiantyn Verenych <kverenych@atlassian.com>
2021-11-29 03:59:29 +0200

    Add routing to the App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
+// modules
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 // components
-import Header from './components/header'
-import Catalogue from './components/catalogue'
-import Footer from './components/footer'
+import Layout from './pages/layout/layout.page'
+import Main from './pages/main/main.page'
+import Shop from './pages/shop/shop.page'
+import About from './pages/about/about.page'
+import Contacts from './pages/contacts/contacts.page'
+import Section from './pages/section/section.page'
+import Product from './pages/product/product.page'
 
 // styles
 import './App.sass'
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Catalogue />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Main />} />
+          <Route exact path="/shop" element={<Shop />} />
+          <Route exact path="/shop/:section" element={<Section />} />
+          <Route exact path="/shop/:section/:productId" element={<Product />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contacts" element={<Contacts />} />{' '}
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
